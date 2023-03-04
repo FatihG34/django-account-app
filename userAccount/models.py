0f54073285cefdf0a24849ea2a5584d2d924dbdf
FatihG34 <fthgnydn34@gmail.com>
2023-03-04 22:50:08 +0300

    help_text added in some fields

--- userAccount/models.py
+++ userAccount/models.py
@@ -8,13 +8,20 @@ class User(AbstractUser):
     # ? username field will be using for identity number
     # bio = models.TextField(null=True, blank=True) #! biography not necessary for e-commerce
     profile_photo = models.ImageField(upload_to='profile_photo/%Y/%m/%d/', null=True,
-                                      blank=True, height_field=None, width_field=None, max_length=None)
-    phone_number_1 = models.CharField(max_length=15)
-    phone_number_2 = models.CharField(max_length=15, null=True, blank=True)
-    address_1 = models.CharField(max_length=150)
-    address_2 = models.CharField(max_length=150, null=True, blank=True)
-    town = models.CharField(max_length=25)
-    city = models.CharField(max_length=60)
+                                      help_text="You should save a photo with sizes 500*600", blank=True, height_field=None, width_field=None, max_length=None)
+    phone_number_1 = models.CharField(
+        max_length=15, help_text="Such as +90 5xx xxx xx xx")
+    phone_number_2 = models.CharField(
+        max_length=15, help_text="Such as +90 5xx xxx xx xx", null=True, blank=True)
+    address_1 = models.CharField(
+        max_length=150, help_text="Enter your address")
+    address_2 = models.CharField(
+        max_length=150, help_text="Enter your address", null=True, blank=True)
+    town = models.CharField(
+        max_length=25, help_text="Enter your Town that your living")
+    city = models.CharField(
+        max_length=60, help_text="Enter your City that your living")
     post_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=60)
+    country = models.CharField(
+        max_length=60, help_text="Enter your Country that your living")
     updated_date = models.DateTimeField(auto_now=True)
