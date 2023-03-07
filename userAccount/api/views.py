bdda5d46a5b1d76a58cc200132501816cfaa0c66
FatihG34 <fthgnydn34@gmail.com>
2023-03-07 11:04:56 +0300

    Update,AllUsers Views added,create mothod modified

--- userAccount/api/views.py
+++ userAccount/api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
-from .serializers import RegisterSerializer
+from rest_framework import generics, status, viewsets
+from .serializers import RegisterSerializer, UpdateUserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -8,3 +10,27 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = serializer.data
+        if Token.objects.filter(user=user):
+            token = Token.objects.get(user=user)
+            data['token'] = token.key
+        else:
+            data['error'] = 'User does not have token. !!!'
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UpdateUserView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UpdateUserSerializer
+    lookup_field = 'username'
+
+
+class AllUsersView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UpdateUserSerializer
