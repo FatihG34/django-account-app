00fdb50332429d9fc45de98f6dc3fe7ad2eaa308
FatihG34 <fthgnydn34@gmail.com>
2023-03-07 11:01:27 +0300

    update-profile and all-user urls added

--- userAccount/api/urls.py
+++ userAccount/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
-from .views import RegisterView
+from .views import RegisterView, UpdateUserView, AllUsersView
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'all-user', AllUsersView, basename='all-user')
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
+    path("update-profile/<str:username>", UpdateUserView.as_view()),
+    path("", include(router.urls)),
     path("auth/", include('django.contrib.auth.urls')),
     path('rest_auth/', include('dj_rest_auth.urls')),
 ]
